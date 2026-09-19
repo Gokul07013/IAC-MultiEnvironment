503bdf6103db9d41410928400ee5781af220dfee
Gokul <106098368+Gokul07013@users.noreply.github.com>
2026-09-19 18:11:02 +0530

    Change S3 bucket name for Terraform backend

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "rm-state-demo"
+    bucket       = "rm-state-demo2"
     key          = "iac-multienv/dev/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
